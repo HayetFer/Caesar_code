1772a184ea23c1b7062dd54309246e81d084670c
Hayet <117082010+HayetFer@users.noreply.github.com>
2022-12-31 13:36:31 +0100

    Update Message.c

--- Message.c
+++ Message.c
@@ -1,3 +1,4 @@
+//Problèmes de mémoire à corriger et de recursion dans shift a partir de 3 essais
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,16 +11,14 @@ struct Message{
 
 //--------------------a
 void alloueMsg (struct Message * msg, int taille){
-    msg = malloc(sizeof(struct Message));
-    
-    msg->texte=malloc(sizeof(char)*taille);
     msg->taille=taille;
-
+    msg->texte=malloc(sizeof(char)*taille);
 }
 //------------------b
 void freeMsg(struct Message *msg){
-    free(msg->texte);
-    free(&msg);
+    msg->texte=NULL;
+    msg->taille=0;
+    msg=NULL;
 }
 void copieMsg(struct Message *msg, struct Message *copie){
     
@@ -31,7 +30,8 @@ void copieMsg(struct Message *msg, struct Message *copie){
 //--------------------a
 //remember how to access message->taille!
  void simplify (struct Message * msg){
-    char* cpy=malloc(sizeof(char)*25);
+    int a = msg->taille;
+   char* cpy=malloc(sizeof(char)*a);
    int i,j;
     for(i=0,j=0;i<25;i++){
         if((65 <= msg->texte[i] && msg->texte[i] <= 90) || (97 <= msg->texte[i] && msg->texte[i] <= 122)){
@@ -41,6 +41,7 @@ void copieMsg(struct Message *msg, struct Message *copie){
     }
     
    msg->texte=cpy;
+   
 }
 
 
@@ -51,26 +52,50 @@ void copieMsg(struct Message *msg, struct Message *copie){
 char shift (char c, int k){
     
     char g = ' ';
-    if(65< (c + k) && (c + k)<90){
-        char g = c + k;
+    if('A'<=c<='Z'){
+        
+        
+        if('A'<= (c + k) && (c+k)<='Z' ){
+       // printf("%d" , c+k);
+        g = c + k;
         return g;
+        
     }
-    else if(96< (c + k) && (c+k)<123 ){
-        char g = c + k;
-        return g;
-    }
-    else if((c + k)>='A' && (c + k)<='Z'){
-      
+    else if((c + k)>'Z'){
+       
         while(c<91){
-            printf(" \n al \n");
+           // printf(" \n al \n");
             c++;
             k--;
         }
         c='A';
-        c+=k;
-        g=c;
-        return g;
+       
+        shift(c,k);
     }
+    }
+
+    if('a'<=c<='z'){
+        
+        
+        if('a'<= (c + k) && (c+k)<='z' ){
+        printf("%d" , c+k);
+        g = c + k;
+        return g;
+        
+    }
+    else if((c + k)>'z'){
+       
+        while(c<91){
+           // printf(" \n al \n");
+            c++;
+            k--;
+        }
+        c='a';
+       
+        shift(c,k);
+    }
+    }
+
     
     return g;
 }
@@ -79,17 +104,17 @@ char shift (char c, int k){
 
 int main(){
     struct Message temp;
-    alloueMsg(&temp, 25);
-    temp.texte = "Hello my name are 9 but";
+    alloueMsg(&temp, 40);
+    strcpy(temp.texte , "hello my98 name is ");
     simplify(&temp);
-   // printf("%s \n",temp.texte);
-    char c = 'g';
-    int b = 1; 
+    printf("%s" , temp.texte);
+    char c = 'v';
+    int b = 28; 
     printf("\n");
     printf("%c \n",shift(c, b));
     struct Message temp2;
     copieMsg(&temp, &temp2);
-    free(&temp);
-    free(&temp2);
+    freeMsg(&temp);
+    freeMsg(&temp2);
     return 0;
 }
